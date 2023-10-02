87e1665d19870ef0f775d33658d4b3ea58e3b7e7
garfbit <104676660+garfbit@users.noreply.github.com>
2023-10-02 13:38:19 -0500

    Archivos iniciales para el proyecto MakeCode

---
+++ main.ts
@@ -0,0 +1,17 @@
+input.onLogoEvent(TouchButtonEvent.Pressed, function () {
+    radio.sendNumber(4)
+    basic.showIcon(IconNames.Square)
+})
+input.onButtonPressed(Button.A, function () {
+    radio.sendNumber(1)
+    basic.showArrow(ArrowNames.West)
+})
+input.onButtonPressed(Button.AB, function () {
+    radio.sendNumber(3)
+    basic.showArrow(ArrowNames.North)
+})
+input.onButtonPressed(Button.B, function () {
+    radio.sendNumber(2)
+    basic.showArrow(ArrowNames.East)
+})
+radio.setGroup(1)
